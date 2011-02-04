85243823c1cfcb04feac350ef489c57db759c5c0
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-02-04 16:37:36 -0300

    Fix: Destination directory creation and checks for it. Correctly call for collector script.

--- simple-mysql-backup.sh
+++ simple-mysql-backup.sh
@@ -11,8 +11,7 @@
 
 MYSQL=/usr/bin/mysql
 MYSQLSHOW=/usr/bin/mysqlshow
-MYSQLDUMP="/usr/bin/mysqldump -e -q -R --compress --create-options --comments"
-AWK=/usr/bin/awk
+MYSQLDUMP="/usr/bin/mysqldump_ -e -q -R --compress --create-options --comments"
 HOSTS="localhost
 192.168.23.1"
 USERNAME="root"
@@ -27,18 +26,35 @@ information_schema"
 
 echo ">> Simple MySQL Backup initialized.";
 
-if [ -a $COMMAND ]; then
+if [ -e $COMMAND ]; then
     /bin/rm $COMMAND;
     echo "Removed old collector script.";
 fi;    
 
 echo "Making a new collector script...";
 for host in $HOSTS; do
+    # Creates the connection string
     CONN="-u $USERNAME -h $host"
+
+    # if password == "" the "-p" parametter is not needed.
     if [ "$PASSWORD" != "" ]; then
         CONN="$CONN -p$PASSWORD"
     fi;
-    for db in `$MYSQLSHOW $CONN | $AWK '{ print $2 }'`; do
+
+    # Will create a subdirectory per host in DEST, if it can.
+    if [ ! -e $DEST/$host ]; then                                                                               
+        echo ${checkdir[$host]}; 
+        echo "/bin/mkdir $DEST/$host;" >> $COMMAND;
+        DESTDUMP=$DEST/$host;
+    elif [ -d $DEST/$host ]; then
+        DESTDUMP=$DEST/$host;
+    else
+        echo "# Cannot create $DEST/$host because already exist and it is not a directory." >> $COMMAND;
+        DESTDUMP=$DEST;
+    fi;
+
+    # Itering over 'show databases()' result.
+    for db in `$MYSQLSHOW $CONN | /usr/bin/awk '{ print $2 }'`; do
         must_ignore=0
         for ignore in $IGNORE; do
             typeset -l test_ignore=$ignore;
@@ -48,25 +64,33 @@ for host in $HOSTS; do
                 break;
             fi;
         done;
-        if [ $must_ignore == 0 ]; then
-            echo "$MYSQLDUMP $CONN $db > $DEST/$db@$host-$DATE.sql;" >> $COMMAND;
+
+        if [ $must_ignore == 1 ]; then
+            # Continue to the next db if must ignore this one.
+            continue;
         fi;
+
+        echo "$MYSQLDUMP $CONN $db > $DESTDUMP/$db@$host-$DATE.sql;" >> $COMMAND;
+
     done;
 done;
 
 echo "Done. Now executing...";
 
-if [ ! -a $COMMAND ]; then
+if [ ! -e $COMMAND ]; then
     echo "** Collector file does not exist. Abort.";
     exit 1;
 fi;
 
 /bin/chmod +x $COMMAND;
-exec $COMMAND;
+$COMMAND;
 
 echo "Done. Now compresing...";
 
-/bin/gzip $DEST/*.sql;
+# Compress all resulting files.
+for dumpfile in `/usr/bin/find $DEST -name '*$DATE.sql' -print;`; do
+    /bin/gzip $dumpfile;
+done;    
 
 echo "Done.";
 echo ">> Finished.";
