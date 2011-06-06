d2be4cc06dc1b540b22ad43e4e9d5ed8c2303084
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-06-06 09:29:46 -0300

    ADD: create environment for work.

---
+++ work-environment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BIN=/usr/bin
+
+cd $BIN;
+
+terminator & # console
+pidgin & skype & # IM clients
+tsclient & # windows remote desktop
+transmission-gtk & # torrent
+google-chrome & # web browser
