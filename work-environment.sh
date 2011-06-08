402c87a164df30179062a013cff663e42c95af86
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-06-08 13:40:51 -0300

    FIX: terminator parameters.

--- work-environment.sh
+++ work-environment.sh
@@ -6,6 +6,7 @@ cd $BIN;
 
 terminator & # console
 pidgin & skype & # IM clients
+clementine & # music player
 tsclient & # windows remote desktop
 transmission-gtk & # torrent
 google-chrome & # web browser
